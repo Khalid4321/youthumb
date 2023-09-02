25b0f7fc44954a6a2fb19c2b0c727c960593c15e
Khalid4321 <pespro027@gmail.com>
2023-09-02 01:02:26 +0100

    _document.js for better SEO

--- pages/_document.js
+++ pages/_document.js
@@ -1,36 +1,36 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-export default MyDocument;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YouTube Thumbnail Downloader</title>
+    <meta name="description" content="Save YT Video Thumbnails - Get free thumbnail image of any YouTube videos in Full HD (1080), HD (720), SD, and also in small size. It's currently supported formats: YouTube (HD, HQ, 1080p, 4K) videos">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+</head>
+<body>
+    <h1>YouTube Thumbnail Downloader</h1>
+    <p>Save YT Video Thumbnails - Get free thumbnail image of any YouTube videos in Full HD (1080), HD (720), SD, and also in small size. It's currently supported formats: YouTube (HD, HQ, 1080p, 4K) videos.</p>
+    
+    <h2>What is the use of this YouTube Thumbnail Grabber website?</h2>
+    <p>People use this YouTube thumbnail downloader website for getting thumbnails from any YouTube videos. They can be used in presentations, animation work, or many other activities.</p>
+    
+    <h2>How to use this YouTube thumbnail downloader website?</h2>
+    <p>Using this YouTube downloader website is easy. First, copy the YouTube link whose thumbnail you want. Then, paste that URL in the input box, and the website will automatically generate different sizes of thumbnails for you. Click on the thumbnail download button, and it will be downloaded to your system.</p>
+    <p>If you are using an Android phone, you can save the images. If you are using an iPhone, the process may be different.</p>
+    
+    <h2>Is it legal to download YouTube thumbnails?</h2>
+    <p>Yes, it's 100% legal to download YouTube video thumbnails. However, keep in mind that both the thumbnail and the video are copyrighted products, so you should seek the author's permission before reusing them.</p>
+    
+    <h2>Compatibility of this YT thumbnail grabber website</h2>
+    <p>This YouTube thumbnail downloader website works well on all devices except iPhones, as iPhones don't allow images to be saved directly. If you are using a jailbroken iPhone, there should be no problem. It works fine on most Android devices, laptops, and desktop systems.</p>
+    
+    <h2>Is there any Copyright risk on YouTube Thumbnails?</h2>
+    <p>The YouTube screenshots you download are copyrighted by the respective owners of the videos. If you want to use them in your work, you should ask for permission. Especially if you plan to reuse them on YouTube, it can create problems. However, for uses outside of YouTube, such as website logos or Photoshop work, there should be no issues.</p>
+    
+    <h2>Is reusing YouTube thumbnails SEO-friendly?</h2>
+    <p>No, reusing YouTube thumbnails is not SEO-friendly because Google indexes almost all thumbnails on YouTube. Google can distinguish between unique and duplicate thumbnails, so it's not recommended for SEO. However, you can make a thumbnail unique again by adding effects using software like Photoshop, making it SEO-friendly once more.</p>
+</body>
+</html>
